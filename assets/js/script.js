3ffe88c54b0bef5d43be09850031c5e9bbd5f803
Dev-Maradona <maradona.0000@yahoo.com>
2022-11-29 01:35:48 +0200

    Finish Service

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
  * add event listener on multiple elements
 */
 
-const addEventOnElements = function(elements, eventType, callback) {
+const addEventOnElements = function (elements, eventType, callback) {
     for (let i = 0, len = elements.length; i < len; i++) {
         elements[i].addEventListener(eventType, callback);
     }
@@ -20,7 +20,7 @@ const addEventOnElements = function(elements, eventType, callback) {
 const preloader = document.querySelector("[data-preloader]");
 const circle = document.querySelector("[data-circle]");
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
     preloader.classList.add("loaded")
     document.body.classList.add("loaded");
 
@@ -40,7 +40,7 @@ const navToglleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = function() {
+const toggleNavbar = function () {
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     navToglleBtn.classList.toggle("active");
@@ -55,10 +55,94 @@ addEventOnElements(navTogglers, 'click', toggleNavbar);
 
 const header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     if (this.scrollY >= 100) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");
     }
 });
+
+
+/**
+ * SLIDER
+*/
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function (currentSlider) {
+
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+    let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+    let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    let currentSlidePos = 0;
+
+    const moveSliderItem = function () {
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+    }
+
+    /**
+     * NEXT SLIDE
+     */
+    const slideNext = function () {
+        const slideEnd = currentSlidePos >= totalSlidableItems;
+
+        if (slideEnd) {
+            currentSlidePos = 0;
+        } else {
+            currentSlidePos++;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderNextBtn.addEventListener("click", slideNext);
+
+    /**
+     * PREVIOUS SLIDE
+     */
+    const slidePrev = function () {
+        if (currentSlidePos <= 0) {
+            currentSlidePos = totalSlidableItems;
+        } else {
+            currentSlidePos--;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderPrevBtn.addEventListener("click", slidePrev);
+
+    const dontHaveExtraItem = totalSlidableItems <= 0;
+    if (dontHaveExtraItem) {
+        sliderNextBtn.style.display = 'none';
+        sliderPrevBtn.style.display = 'none';
+    }
+
+    /**
+     * slide with [shift + mouse wheel]
+     */
+
+    currentSlider.addEventListener("wheel", function (event) {
+        if (event.shiftKey && event.deltaY > 0) slideNext();
+        if (event.shiftKey && event.deltaY < 0) slidePrev();
+    });
+
+    /**
+     * RESPONSIVE
+     */
+
+    window.addEventListener("resize", function () {
+        totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+        totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+        moveSliderItem();
+    });
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
